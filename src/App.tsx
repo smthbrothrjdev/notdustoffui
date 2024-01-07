4c6e9835287bcee4c56e132a19564a102ddbc1d8
Bj Jackson <smthbrothrjdev@gmail.com>
2024-01-06 19:20:35 -0600

    diag custom component wierdness in consumer 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import { Button, buttonVariants} from './components/ui/button.tsx'
+import { Button, ButtonProps} from "notdustoffui-package";
 
 function App() {
   const [count, setCount] = useState(0)
